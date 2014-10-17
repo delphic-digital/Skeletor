84423fdbffa1f933570b6c6086f71d458625c367
jefftherobot <jeffy9@gmail.com>
2014-10-17 13:49:15 -0400

    Don't need bump comment anymore

--- gruntfile.js
+++ gruntfile.js
@@ -128,15 +128,3 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('default', 'dev');
 };
-
-/*
-X.Y.Z
-
-1. Z is increasing if a release contains just a bug fix, dependencies update or some application internal changes, so no new functionality introduced to user;
-2. Y is increasing if a release contains minor changes in UI or just introduces some new feature, or some feature redesign/functionality update which will affect user;
-3. X is increasing if a release contains major UI/functionality update, for example: new interface design/layout;
-
-New project starts with version 0.1 and minor version is increasing with development;
-If a project contains all planned functionality for version 1.0 the version should be changed to 1.0.
-
-*/
