55add90342b38e3f01f10cf2a8c009b89991fdb3
Jeff <jmills@delphicdigital.com>
2015-09-24 17:06:09 -0400

    Fix browser-update options scrope

--- Static/src/js/utils/browser-update.js
+++ Static/src/js/utils/browser-update.js
@@ -1,6 +1,8 @@
 //http://browser-update.org/
 
 define(function () {
-	var $buoop = {vs:{i:9,f:5,o:12.1,s:6},c:2}; //IE <=9  Firefox <= 5  Opera <= 12.1  Safari <= 6  Chrome:  auto
+	window.$buoop = {
+		url: "http://browsehappy.com/"
+	};
 	require(['//browser-update.org/update.min.js'])
 });
