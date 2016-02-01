1b6899c40029f9113d39ed37a3e5a1c85a595692
Jeff <jmills@delphicdigital.com>
2016-02-01 15:29:24 -0500

    Started gulp convert

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    nodeSassGlobbing = require('node-sass-globbing'),
+    bourbon =
+
+gulp.task('sass', function () {
+	return gulp.src('./Static/src/scss/main.scss')
+		.pipe(sass({
+			importer: nodeSassGlobbing,
+			includePaths:[].concat(require('bourbon').includePaths, './node_modules/susy/sass', './node_modules/breakpoint-sass/stylesheets'),
+			outputStyle: 'compressed'
+	}).on('error', sass.logError))
+	.pipe(gulp.dest('./Static/dist/css'));
+});
