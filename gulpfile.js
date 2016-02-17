64ab67209a5b93556772b523766d5f0c882eebdb
Jeff <jmills@delphicdigital.com>
2016-02-17 17:11:45 -0500

    Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    nodeSassGlobbing = require('node-sass-globbing'),
-    bourbon =
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass');
 
+//Compile SASS
 gulp.task('sass', function () {
 	return gulp.src('./Static/src/scss/main.scss')
 		.pipe(sass({
-			importer: nodeSassGlobbing,
+			importer: require('node-sass-globbing'),
 			includePaths:[].concat(require('bourbon').includePaths, './node_modules/susy/sass', './node_modules/breakpoint-sass/stylesheets'),
 			outputStyle: 'expanded'
 	}).on('error', sass.logError))
-	.pipe(gulp.dest('./Static/dist/css'));
+	.pipe(gulp.dest('./Static/dist/css'))
+	.pipe(browserSync.stream());
 });
+
+//Browser Sync
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+//Watch
+gulp.task('dev',['browserSync', 'sass'], function(){
+	gulp.watch('./Static/src/scss/**/*.scss', ['sass']);
+	gulp.watch('./*.html', browserSync.reload);
+	gulp.watch('./Static/src/js/**/*.js', browserSync.reload);
+})
