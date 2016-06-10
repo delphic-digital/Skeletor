6361f6e8a5b5b3dfcf92ac930a301281b1b24459
Jeff Mills <jeffy9@gmail.com>
2016-06-09 21:15:07 -0400

    remove bower includes from require

--- Static/src/js/skeletor.main.js
+++ Static/src/js/skeletor.main.js
@@ -1,11 +1,6 @@
 // Automatically injected Bower JS dependencies via bowerRequireJS
 require.config({
 	paths: {
-		"delphic.browserupdate": "../../../bower_components/delphic.browserupdate/delphic.browserupdate",
-		"delphic.loader": "../../../bower_components/delphic.loader/delphic.loader",
-		onMediaQuery: "../../../bower_components/onMediaQuery/js/onmediaquery",
-		picturefill: "../../../bower_components/picturefill/dist/picturefill",
-		svg4everybody: "../../../bower_components/svg4everybody/dist/svg4everybody"
 	},
 	packages: [
 
