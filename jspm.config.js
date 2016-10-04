6c0fe202cb9190f578061c89a209da68d8486f1e
Jeff <jmills@delphicdigital.com>
2016-10-04 11:10:57 -0400

    npm init and jspm init

---
+++ jspm.config.js
@@ -0,0 +1,41 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/"
+  },
+  browserConfig: {
+    "baseURL": "/",
+    "paths": {
+      "skeletor/": "src/"
+    }
+  },
+  nodeConfig: {
+    "paths": {
+      "skeletor/": "Static/src/js/"
+    }
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.16"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "skeletor": {
+      "main": "skeletor.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
