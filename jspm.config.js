fb0c4c99746ed82a3a8e4f896f9fd5ee3ec796b1
Jeff <jmills@delphicdigital.com>
2016-11-21 16:15:30 -0500

    Getting sprites working with npm scripts

--- jspm.config.js
+++ jspm.config.js
@@ -32,12 +32,12 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
-    "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
+    "child_process": "npm:jspm-nodelibs-child_process@0.2.0",
     "jquery": "npm:jquery@3.1.1",
     "logdown": "npm:logdown@1.4.2",
     "onMediaQuery": "github:JoshBarr/on-media-query@0.2.0",
     "picturefill": "npm:picturefill@3.0.2",
-    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "process": "npm:jspm-nodelibs-process@0.2.0",
     "svg4everybody": "npm:svg4everybody@2.1.3"
   },
   packages: {}
