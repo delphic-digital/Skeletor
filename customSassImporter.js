21276d9e633f50679bf6838aed1367168d5cdb48
Jeff <jmills@delphicdigital.com>
2016-12-06 12:33:46 -0500

    JSPM sass importer working

---
+++ customSassImporter.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var jspmImporter = require('./node_modules/sass-jspm-importer/src/importer');
+var magicImporter = require('./node_modules/node-sass-magic-importer/dist/cli');
+
+module.exports = [jspmImporter, magicImporter];
