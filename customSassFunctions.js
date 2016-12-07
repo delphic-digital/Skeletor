870900f89a02ac4feca2160d4f77396d104412f4
Jeff <jmills@delphicdigital.com>
2016-12-07 11:14:41 -0500

    Move custom npm script functions

--- customSassFunctions.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var inliner = require('./node_modules/sass-inline-svg');
-
-module.exports = {
-  'svg': inliner('./Static/src/sprites/vectors', {optimize: true})
-};
--- customSassImporter.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var jspmImporter = require('./node_modules/sass-jspm-importer/src/importer');
-var magicImporter = require('./node_modules/node-sass-magic-importer/dist/cli');
-
-module.exports = [jspmImporter, magicImporter];
