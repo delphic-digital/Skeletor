ca139faf7bdc57e269bc392e1cb7ba20127d386e
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-10-24 11:03:59 -0400

    png sprite

--- build_tasks/png_sprite.js
+++ build_tasks/png_sprite.js
@@ -1,27 +1,24 @@
 var gulp = require('gulp'),
-    config = require('../config'),
     spritesmith = require('gulp.spritesmith'),
     mergeStream = require('merge-stream');
 
-
 /**
-* This creates a _sprites.scss file that sits in with the rest of the sass
+* This ...
 */
+
 gulp.task('png_sprite', function () {
-    var spriteData = gulp.src(config.devSpriteFilesDir + 'bitmaps/**/*.png').pipe(spritesmith({
-        retinaSrcFilter: config.devSpriteFilesDir + 'bitmaps/**/*@2x.png',
-        imgName: 'bitmap.spritesheet.png',
-        retinaImgName: 'bitmap.spritesheet@2x.png',
-        imgPath: '../../assets/spritesheets/bitmap.spritesheet.png',
-        retinaImgPath : '../../assets/spritesheets/bitmap.spritesheet@2x.png',
-        cssName: '_sprites.scss'
-    }));
+    var spriteData = gulp.src(`${global.skeletor.srcPngDir}/**/*.png`)
+        .pipe(spritesmith({
+            imgName: 'pngSprite.png',
+            imgPath: `${global.skeletor.distCssPngSpriteDirUrl}/pngSprite.png`,
+            cssName: '_png.scss'
+        }));
 
     var imgStream = spriteData.img
-        .pipe(gulp.dest(config.compileSpriteFilesDir));
+        .pipe(gulp.dest(global.skeletor.distSpritePngDir));
 
     var cssStream = spriteData.css
-        .pipe(gulp.dest(config.devSassFilesDir + 'partials/_svgenerated/'));
+        .pipe(gulp.dest(`${global.skeletor.srcScssDir}/abstracts/`));
 
     return mergeStream(imgStream, cssStream);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 
+//TODO: browser sync streams
+//TODO: postcss plugins
+//TODO: documentation!
+
 //Keeping all the fed development files together, wherever a project needs them to be
 const fedSrcRoot = './src';
 
@@ -14,6 +18,7 @@ global.skeletor = {
     distSpriteSvgDir: './dist/sprite',
     distSpritePngDir: './dist/sprite',
     proxy: 'project.dev', //The local url where the 'real' version of the site is running
+    distCssPngSpriteDirUrl: '../../assets/spritesheets' //for the dist css's url('...') - will be specific to each project
 };
 
 require('./build_tasks/browserSync.js');
@@ -21,29 +26,18 @@ require('./build_tasks/sass.js');
 require('./build_tasks/js.js');
 require('./build_tasks/svg_sprite.js');
 require('./build_tasks/svg_inlinecss.js');
-// require('build_tasks/png_sprite.js');
+require('./build_tasks/png_sprite.js');
 
 //Watching you work
 gulp.task('watch', function(){
-    gulp.watch(`${global.skeletor.srcScssDir}/**/*.scss`, gulp.series('sass'));
-    gulp.watch(`${global.skeletor.srcJsDir}/**/*.js`, gulp.series('js'));
-    gulp.watch(`${global.skeletor.srcSvgDir}/**/*.svg`, gulp.series('svg_sprite', 'svg_inlinecss'));
-    
-    // //Sprites compile into sass, a cshtml file, and I believe a few assets for the site to load
-    // //so we need to compile it, run the sass to pick that up, copy the css, copy the assets (I believe), and copy the views in
-    // gulp.watch(config.devSpriteFilesDir, gulp.series('sprite:build', 'sass', 'copy:css', 'copy:assets', 'copy:views', 'browserSync:reload'));
-  
-    // //any html updates - don't want to do a build so just copy the cshtml files in and hope for the best!
-    // gulp.watch(config.devViewsDir, gulp.series('copy:views','browserSync:reload'));
-  
-    // //currently the js optimization takes ages so for development we're just using the src js.
-    // //this is also copying the dev js so after a build we can start devving again and see it (with npm start or npm run magic-once)
-    // //(search the project for "var useDevJs =" and toggle that to switch version of the js is run on your local - don't commit that change though!)
-    // gulp.watch(config.devJsFilesDir + '/**/*.js', gulp.series('copy:js', 'copy:devjs', 'browserSync:reload'));
+    gulp.watch(`${global.skeletor.srcScssDir}/**/*.scss`, gulp.series('sass', 'browserSync:reload'));
+    gulp.watch(`${global.skeletor.srcJsDir}/**/*.js`, gulp.series('js', 'browserSync:reload'));
+    gulp.watch(`${global.skeletor.srcSvgDir}/**/*.svg`, gulp.series('svg_sprite', 'svg_inlinecss', 'browserSync:reload'));
+    gulp.watch(`${global.skeletor.srcPngDir}/**/*.png`, gulp.series('png_sprite', 'browserSync:reload'));
 });
 
 //build all the files!
-gulp.task('build', gulp.series('js', 'svg_sprite', 'svg_inlinecss', 'sass'));
+gulp.task('build', gulp.series('js', 'svg_sprite', 'svg_inlinecss', 'png_sprite', 'sass'));
 
 //default task watches
 gulp.task('default', gulp.series('build', 'browserSync', 'watch'));
