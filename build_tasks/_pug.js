523314394337aca7575fb25296969a3c7c99498c
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-10-31 09:27:16 -0400

    Static templating inheritance

--- build_tasks/_pug.js
+++ build_tasks/_pug.js
@@ -4,7 +4,7 @@ var pug = require('gulp-pug');
 gulp.task('pug', function buildHTML() {
 	return gulp.src(`${global.skeletor.srcPugDir}/*.pug`)
 		.pipe(pug({
-			
+			pretty: '\t'
 		}))
 		.pipe(gulp.dest(global.skeletor.distPugDir));
 });
