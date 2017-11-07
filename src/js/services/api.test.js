6adecf6785d7f69cb3b0927e1394202c58a9c64d
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-11-07 07:37:21 -0500

    playing with json schema error output

--- src/js/services/api.test.js
+++ src/js/services/api.test.js
@@ -5,7 +5,7 @@ import schema from 'validate'; //https://www.npmjs.com/package/validate
 const api = request('https://jsonplaceholder.typicode.com'); //https://jsonplaceholder.typicode.com/
 const postSchema = schema({
 	'userId': {
-		type: 'number',
+		type: 'object',
 		required: true,
 		message: 'userId is required'
 	},
@@ -29,10 +29,10 @@ const postSchema = schema({
 test.cb('api /posts/1', (t) => {
 	t.plan(1);
 
-	api.get('/posts/1')
+	api.get('API JSON should match this Schema')
 		.end(function (err, res) {
 			let schemaErrors = postSchema.validate(res.body);
-			t.true(schemaErrors.length == 0, schemaErrors);
+			t.true(schemaErrors.length == 0, 'post object schema error: ' + schemaErrors);
 			t.end();
 		});
 });
