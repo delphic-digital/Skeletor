9e4e7617358c82eb65f11c2a5bc9b0f165af2af8
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-11-08 07:23:56 -0500

    looking into error handling

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,22 @@
 import $ from 'jquery';
 import hello from './components/hello.js'; //simple component in it's oen js file, give it the .js extension
 import flipper from './components/flipper'; //complex component contained in a folder, just name the folder
+import errorExample from './components/errorExample.js';
 
+//jQuery everywhere!
 window.$ = $;
+
+//Global error listener, use services/SkeletError to throw better errors
+window.addEventListener('error', function (e) {
+	var error = e.error;
+	console.log(error, e);
+});
+
+//here's an example of SkeletError in action!
+errorExample();
+
 
 $(document).ready(function() {
 	flipper.init();
-		hello.init();
+	hello.init();
 });
