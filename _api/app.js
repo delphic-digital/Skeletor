12c46ec8f4ad71a48457e2103367bbd267bf9842
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-12-12 15:54:17 -0500

    data file for api responce

--- _api/app.js
+++ _api/app.js
@@ -4,17 +4,25 @@ var restify = require('restify');
  * A simple API server to mock data
  */
 
-function respond(req, res, next) {
+var api = restify.createServer();
+var mockData = require('./data.json');
+
+api.use(restify.plugins.queryParser());
+api.use(restify.plugins.bodyParser());
+// api.use(restify.throttle({burst:100,rate:50,ip:true}));
+
+api.get('/', function(req, res, next) {
+	res.json(mockData);
+	next();
+});
+
+api.get('/hello/:name', function(req, res, next) {
 	res.json({
 		'hello': req.params.name
 	});
 	next();
-}
-
-var api = restify.createServer();
-api.get('/hello/:name', respond);
-api.head('/hello/:name', respond);
+});
 
 api.listen(9001, function() {
-	console.log('%s listening at %s', api.name, api.url);
+	console.log('API listening: ', api.url);
 });
