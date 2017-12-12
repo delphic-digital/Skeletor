bff0da700982ceb6f3bedbe68e472dee6fe53efb
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-12-12 16:06:35 -0500

    params to json fun route

--- _api/app.js
+++ _api/app.js
@@ -16,6 +16,13 @@ api.get('/', function(req, res, next) {
 	next();
 });
 
+//Turns query params into JSON and fires it right back at you
+//eg http://localhost:9001/params?key1=value1&key2=value2 returns {key1: "value1",key2: "value2"}
+api.get('/params', function(req, res, next) {
+	res.json(req.query);
+	next();
+});
+
 api.get('/hello/:name', function(req, res, next) {
 	res.json({
 		'hello': req.params.name
